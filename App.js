685733134b08598da3d3d81edb0ec9586f70398d
Linsted <ivan.malenko18@gmail.com>
2023-08-26 22:53:09 +0300

    feat: add first styles

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>hello world</Text>
+      <Text style={styles.text}>hello world  #2255</Text>
+      <Button style={{ backgroundColor: '#cccfff' }} title='Tap me' />
+      <Button style={{ color: "red" }} title='Don`t tap me' />
     </View>
   );
 }
@@ -17,4 +18,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    marginBottom: 15,
+    color: 'red',
+    padding: 5,
+    borderColor: 'blue',
+    borderWidth: 1,
+
+  }
 });
